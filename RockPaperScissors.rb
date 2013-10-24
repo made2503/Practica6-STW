87c494d26af613175bf17dde7e56f3d61d9ce7e7
Madelaine Martin <made_2503@hotmail.com>
2013-10-24 17:09:57 +0100

    Probando

--- RockPaperScissors.rb
+++ RockPaperScissors.rb
@@ -1,21 +1,4 @@
 require './lib/RockPaperScissors'
 
-
-builder = Rack:: Builder.new do
-	use Rack::Static, :urls => ['/public']
-	use Rack::ShowExceptions
-	use Rack::Lint
-	use Rack::Session::Cookie,
-		{:key => 'rack.session',
-		:domain => 'rps.com',
-		:secret => 'cookie'}
-
-	run RockPaperScissors::RPS.new
-end
-
-use Rack::Server.start(
-	:app => builder,
-	:Port => 8080,
-	:server => 'thin'
-	)
+run RockPaperScissors::RPS
 
